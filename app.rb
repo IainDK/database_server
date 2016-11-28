50823de08841b694a82b52f88c69aca3f14e33f1
Iain Kinnish <iainkinnish@googlemail.com>
2016-11-28 15:23:28 +0000

    Query strings can be manually entered by visiting '/set', the data is saved and can be viewed at '/get'. Both tests are passing.

--- app.rb
+++ app.rb
@@ -8,6 +8,16 @@ class DatabaseServer < Sinatra::Base
     "We're live!"
   end
 
+  get '/set' do
+    parameters = request.query_string.split("=")
+    session[:somekey] = parameters[0]
+    session[:somevalue] = parameters[1]
+  end
+
+  get '/get' do
+    session[:somekey]
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/feature/database_server_spec.rb
+++ spec/feature/database_server_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 feature 'The application should be hosted on port 4000.' do
-  scenario 'Visiting Homepage' do
+  scenario 'The application should be live.' do
     visit('/')
     expect(page.status_code).to eq(200)
     expect(page).to have_content("We're live!")
+  end
+end
+
+feature 'You can manually enter a key and value into a query string.' do
+  scenario 'Key data is saved and viewable.' do
+    visit('/set?Colour=Blue')
+    visit('/get')
+    expect(page).to have_content('Colour')
   end
 end

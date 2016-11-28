941cf44aa841398b6436144b44b9d088bde28fb6
Iain Kinnish <iainkinnish@googlemail.com>
2016-11-28 15:28:41 +0000

    Refined first test to ensure that the site is being hosted on the correct port.

--- spec/feature/database_server_spec.rb
+++ spec/feature/database_server_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'The application should be hosted on port 4000.' do
   scenario 'The application should be live.' do
-    visit('/')
+    visit('http://localhost:4000')
     expect(page.status_code).to eq(200)
     expect(page).to have_content("We're live!")
   end
